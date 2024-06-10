947886dd4dbd3a3c1ed28edbf45baef9f0245721
Lvov Alexandr <strangeisdoctor@gmail.com>
2024-06-10 16:42:28 +0300

    added interaction with database and scheduler

--- bot.py
+++ bot.py
@@ -1,12 +1,15 @@
 import asyncio
+import logging
 
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.methods import DeleteWebhook
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 from config import TOKEN_BOT
 from bot_interaction import base
+from database.db import create_db
 
 class UserState(StatesGroup):
     city_choice = State()
@@ -16,9 +19,18 @@ bot = Bot(token=TOKEN_BOT)
 
 
 async def main():
+    logging.basicConfig(level=logging.INFO)
+
     storage = MemoryStorage()
     dp = Dispatcher(storage=storage)
     dp.include_routers(base.router)
+
+    create_db()
+    
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(base.send_weather_to_all, 'cron', hour='10,12,14,16,18,20,22', minute='41')
+    scheduler.start()
+
     await bot(DeleteWebhook(drop_pending_updates=True))
     await dp.start_polling(bot)
 
---
+++ database/db.py
@@ -0,0 +1,35 @@
+import json
+
+def create_db():
+    with open("db.json", "w") as f:
+        json.dump({"users_data": []}, f)
+
+
+def get_users_data():
+    with open ("db.json", "r") as f:
+        data = json.load(f)
+        return data["users_data"]
+
+
+def add_user_info(info: dict):
+    # info represents a dictionary with keys tg-id and city of this user
+    with open("db.json", "r+") as f:
+        data = json.load(f)
+        if (check_iser_exists(info["telegram_id"])):
+            print("User exists!")
+            return
+        data["users_data"].append(info)
+        f.seek(0)
+        json.dump(data, f, ensure_ascii=False)
+
+
+def check_iser_exists(id):
+    with open("db.json", "r") as f:
+        data = json.load(f)
+        for d in data["users_data"]:
+            if id == d['telegram_id']:
+                return True
+        return False
+
+
+
